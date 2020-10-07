14ec7e8b588cb8dc3b45a5384ae228cd0d25428a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-06 21:10:32 -0400

    Include first file

---
+++ Day1/main.js
@@ -0,0 +1,17 @@
+
+//Counting sheep...
+//Consider an array/list of sheep where some sheep may be missing from their place.
+//We need a function that counts the number of sheep present in the array (true means present).
+
+let listOfSheep = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+listOfSheep.forEach(functon(sheep){
+  if (sheep === true) {
+    console.log(sheep)
+  }
+})
