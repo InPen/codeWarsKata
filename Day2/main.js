6a256e6925850d434b57dda201820219443d6ea4
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-08 10:52:19 -0400

    What I think is right before testing

--- Day2/main.js
+++ Day2/main.js
@@ -1,8 +1,31 @@
 //Write a function, persistence, that takes in a positive parameter num
-function persistence(num) {
+function persistence(number) {
+//turn number into a string so we can check how many digits it has
+let num = '' + number
 //how many times must you multiply the digits in num until you reach a single digit.
-if(num //is more than 1 digit){
-  //multiply each num
-} else if(num is single digit)
-  //returns its multiplicative persistence RETURN SINGLE DIGIT
+if(num.length > 1){
+  //multiply all digits of num
+  num = num.split("")
+  num = multiply(num)
+} else if(num.length === 1)
+  //returns its multiplicative persistence
+  console.log(`${number} Multiplicative Persistence is ${num}`)
 }
+persistence(36)
+
+// function MultiplicativePersistence(num) {
+//
+//        function multiply(n){
+//            return n.reduce(function(a,b){return a*b;});
+//        }
+//        var count =0;
+//
+//        while(num.toString().length > 1) {
+//            num= num.toString().split("");
+//            num = multiply(num);
+//            count++;
+//         }
+//         return count;
+//     }
+//
+// alert(MultiplicativePersistence("39"));
