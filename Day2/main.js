9a709bed87d3bf69df7945bf364225bce39bafa5
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-08 10:04:20 -0400

    Include Sudo code

--- Day2/main.js
+++ Day2/main.js
@@ -1,5 +1,8 @@
 //Write a function, persistence, that takes in a positive parameter num
 function persistence(num) {
-//returns its multiplicative persistence,
-//which is the number of times you must multiply the digits in num until you reach a single digit.
+//how many times must you multiply the digits in num until you reach a single digit.
+if(num //is more than 1 digit){
+  //multiply each num
+} else if(num is single digit)
+  //returns its multiplicative persistence RETURN SINGLE DIGIT
 }
