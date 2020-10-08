568311d926a819835800ff78ebba45dcf8ccf042
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-08 09:59:52 -0400

    Add today's kata

---
+++ Day2/main.js
@@ -0,0 +1,5 @@
+//Write a function, persistence, that takes in a positive parameter num
+function persistence(num) {
+//returns its multiplicative persistence,
+//which is the number of times you must multiply the digits in num until you reach a single digit.
+}
