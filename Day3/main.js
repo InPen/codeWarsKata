eed2f2dd348b6601c747754519a3a60087626bec
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-09 15:22:10 -0400

    Intentional empty commit

---
+++ Day3/main.js
@@ -0,0 +1,3 @@
+//Given two integers a and b, which can be positive or negative,
+//find the sum of all the numbers between including them too and return it.
+//If the two numbers are equal return a or b.
