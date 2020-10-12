38094c80c451aed7eb3393b196b1a16797b20776
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-12 12:34:00 -0400

    Include sudo code

--- Day3/main.js
+++ Day3/main.js
@@ -1,3 +1,30 @@
 //Given two integers a and b, which can be positive or negative,
 //find the sum of all the numbers between including them too and return it.
 //If the two numbers are equal return a or b.
+function findSum(a,b){
+  let fullArr = []
+// push a & b into fullArr
+  fullArr.push(a , b)
+  console.log(fullArr)
+  // if a && b === a && b don't add just return a || b
+  //find the sum of all the numbers between including them too and return it.
+}
+findSum(2,4)
+// function sumAll(arr) {
+//
+//     let fullArr = []
+//     let sum = 0
+//     const reducer = (accumulator, currentValue) => accumulator + currentValue
+//
+//     arr.sort(function(a, b) {
+//         return a - b
+//     })
+//
+//     for (let i = arr[0]; i <= arr[1]; i++) {
+//         fullArr.push(i)
+//     }
+//
+//     sum = fullArr.reduce(reducer)
+//
+//     return sum
+// }
