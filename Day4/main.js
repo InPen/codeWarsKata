8e37eef4f767fee94566ce1ff934740bafc60b7a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-13 10:59:56 -0400

    Include solution

--- Day4/main.js
+++ Day4/main.js
@@ -1,5 +1,15 @@
 //Complete the solution so that it reverses the string passed into it.
 //'world'  =>  'dlrow'
-function reverse(string){
-  
+function stringReverse(string){
+// divides the letters of the string and puts them in their own array index
+  let split = string.split('')
+//switches the order of the array
+  let reverse = split.reverse()
+//puts the letters back together as a string
+  let reJoin = reverse.join('')
+
+  console.log(reJoin);
+  return reJoin
+
 }
+stringReverse('Paralelepipedo')
