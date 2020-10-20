3b9dcda8203ea55f53a9b617aa2b78e8bbf1aa11
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-20 15:21:53 -0400

    Include solution

--- Day6/main.js
+++ Day6/main.js
@@ -3,5 +3,8 @@
 // For example:
 //
 // Given [34, 15, 88, 2] your solution will return 2
-// Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+let array = [34, -345, -1, 100]
+const min = Math.min(...array)
+console.log(`The smallest digit in your array is ${min}`)
