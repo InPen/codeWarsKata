748284c3811dcb8c4713054d0b77b9685d50b33f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-29 11:42:07 -0400

    Include today's kata

--- Day7/main.js
+++ Day7/main.js
@@ -1,11 +1,10 @@
-//There is a bus moving in the city, and it takes and drop some people in each bus stop.
+//Complete the method that takes a boolean value and
+//return a "Yes" string for true, or a "No" string for false.
 
-//You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
-
-//Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
-
-//Take a look on the test cases.
-
-//Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
-
-//The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+let millenialCheck = prompt("Enter your age if you want to know if you're a Millenial")
+let millenial = [22, 23, 24 ,25 ,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
+if(millenial == true){
+  console.log('Yes')
+} else if(millenial == false){
+  console.log('No')
+}
