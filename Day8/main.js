c0c0b8adf027a41aa193ea5700318f297d2986df
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-30 11:09:09 -0400

    `{` unexpected token frustration

---
+++ Day8/main.js
@@ -0,0 +1,22 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+//Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+const friends = ['Heba', 'Johann', 'Celia', 'Maria', 'Jess',  'Ryan', 'Leon']
+let stringLength = 4
+
+friends.forEach = friend(string){
+  if(string.length < 4){
+    console.log(string)
+  }
+}
+
+
+
+
+
+// if(friends === stringLength){
+//   console.log(`${friends} is my friend`)
+// } else if(friends !== stringLength){
+//   console.log(`${friends} is a fake poser`)
+// }
