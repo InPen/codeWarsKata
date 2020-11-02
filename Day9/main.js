794e575c7ec7c067e1f9ed8de64e54e4029f4c30
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-02 12:19:57 -0500

    Include file and problem

---
+++ Day9/main.js
@@ -0,0 +1,5 @@
+//Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others.
+//Bob observed that one number usually differs from the others in evenness.
+//Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+//indexes of the elements start from 1 (not 0)
+//**is it even and whats its index
