11a97609231e19c8ce013f4a85cbcfadbd9dcc65
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-05 09:36:19 -0500

    Include problem and sudo code

---
+++ Day10/main.js
@@ -0,0 +1,15 @@
+//Some people have been killed!
+//You have managed to narrow the suspects down to just a few. Luckily, //you know every person who those suspects have seen on the day of the //murders.
+
+//Task:
+//Given a dictionary with all the names of the suspects and everyone //that they have seen on that day which may look like this:
+//return the name of the one killer, in our case 'James' because he is //the only person that saw both 'Lucas' and 'Bill'
+
+const suspectInfo = {'James': ['Jacob', 'Bill', 'Lucas'],
+                      'Johnny': ['David', 'Kyle', 'Lucas'],
+                      'Peter': ['Lucy', 'Kyle']}
+const dead = ['Lucas', 'Bill']
+
+function killer(suspectInfo, dead) {
+  
+}
