d012559dd1c928a87af183fddfc92b3f82f9a4b8
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-05 09:59:54 -0500

    How far I got in 30 minutes

--- Day10/main.js
+++ Day10/main.js
@@ -5,11 +5,16 @@
 //Given a dictionary with all the names of the suspects and everyone //that they have seen on that day which may look like this:
 //return the name of the one killer, in our case 'James' because he is //the only person that saw both 'Lucas' and 'Bill'
 
-const suspectInfo = {'James': ['Jacob', 'Bill', 'Lucas'],
-                      'Johnny': ['David', 'Kyle', 'Lucas'],
-                      'Peter': ['Lucy', 'Kyle']}
+const suspectInfo = {james: ['Jacob', 'Bill', 'Lucas'],
+                      johnny: ['David', 'Kyle', 'Lucas'],
+                      peter: ['Lucy', 'Kyle']}
 const dead = ['Lucas', 'Bill']
 
 function killer(suspectInfo, dead) {
-  
+//I should compare 'suspectInfo' arrays to 'dead' array and return the
+//array who has all the values of dead array
+  if(suspectInfo.james === dead) && || if(suspectInfo.johnny === dead) &&|| if(suspectInfo.peter === dead) {
+    console.log('you killer')
+  }
 }
+killer()
