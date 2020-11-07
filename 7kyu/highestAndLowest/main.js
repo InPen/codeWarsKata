a543e0627baa14dce5e746069d942db9b1f0c5da
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-07 11:13:00 -0500

    Looked for a solution online but I like what I have solved so far I just need to put max and min into a new array and return the highest number first, separated by a single space from the lowest I'm sooooo close 0.0

--- 7kyu/highestAndLowest/main.js
+++ 7kyu/highestAndLowest/main.js
@@ -7,7 +7,13 @@ function highAndLow(numbers){
 //turn the strings into integers
   let numberArray = stringArr.map(el=>parseInt(el))
   console.log(numberArray) // [ 1, 2, 3, 4, 5 ]
-  console.log(Math.min(...numberArray)) // log lowest num //1
-  console.log(Math.max(...numberArray)) //log highest num //5
+// assign the lowest value to min
+  let min = (Math.min(...numberArray))
+  console.log(min) // log lowest num //1
+// assign the highest value to max
+  let max = (Math.max(...numberArray))
+  console.log(max) //log highest num //5
+//put max and min into a new array and return the highest first separated by a single space from the lowest
+  return ((max + min ).toString(/' '+/))
 }
 highAndLow(numbers)
