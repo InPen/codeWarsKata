0b7f7eead088c83af3279651ddc8684f1cdf99f8
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-08 17:10:09 -0500

    Used endsWith method that i found on my first search Need to memorize this method by heart!

---
+++ 7kyu/stringEndsWith/main.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+let str ='abc'
+let ending = 'bc'
+
+
+function solution(str, ending){
+  return str.endsWith(ending)
+}
