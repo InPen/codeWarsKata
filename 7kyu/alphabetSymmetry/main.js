3e8aa34bf0517c952d36dc133ebe2f5c3c22a078
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-10 10:18:43 -0500

    Include sudo code

---
+++ 7kyu/alphabetSymmetry/main.js
@@ -0,0 +1,8 @@
+//Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+//Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+function solve(arr){
+//iterate through array starting at 1
+for(let i = 1; i < arr.length; i++)
+}
