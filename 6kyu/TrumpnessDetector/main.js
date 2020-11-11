db751da14d30593e147b765dd9ee922243bf7e78
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-11 10:37:15 -0500

    Include problem && sudo code

---
+++ 6kyu/TrumpnessDetector/main.js
@@ -0,0 +1,13 @@
+//create a function to detect the original and unique rythm of our beloved leader, typically having a lot of extra vowels, all ready to fight the estabilishment.
+
+//The index is calculated based on how many vowels are repeated more than once in a row and dividing them by the total number of vowels a petty enemy of America would use.
+
+//Notes: vowels are only the ones in the patriotic group of "aeiou": "y" should go back to Greece if she thinks she can have the same rights of true American vowels; there is always going to be at least a vowel, as silence is the option of coward Kenyan/terrorist presidents and their friends.
+
+//Round each result by two decimal digits: there is no place for small fry in Trump's America.
+
+const trumpySpeech = "MEXICAAAAAAAANS GOOOO HOOOMEEEE"
+
+function trumpDetector(trumpySpeech){
+  //your code here
+}
