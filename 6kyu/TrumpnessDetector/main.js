838a07cbf41387f1ec8329241ca36655c54117d2
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-11 13:13:22 -0500

    Johann's peer programming in progress

--- 6kyu/TrumpnessDetector/main.js
+++ 6kyu/TrumpnessDetector/main.js
@@ -6,29 +6,67 @@
 
 //Round each result by two decimal digits: there is no place for small fry in Trump's America.
 
-const trumpySpeech = "MEXICAAAAAAAANS GOOOO HOOOMEEEE"
-let vowels = 'aeiou'
+const mexicans = "MEXICAAAAAAAANS GOOOO HOOOMEEEE"
 
 
-//count the vowels
+// 2 totals which we will late divide and round to 2 decimals
+// 1st total: Count repeats of vowels when they are in a row (not including the vowel itself)
+// ---> check that is repeats
+// ---> check that it is a vowel
+// ----------> if (repeat and vowel)
+// ----------> then save number of repeats - 1
+// 2nd total: Count vowels a normal personal would use in the sentence
+// ---> loop through string again
+// ---> check if index is vowel
+// ---> check if vowel repeats, keep checking until it stops
+// ---> when stop repeating, add += total
+
 function trumpDetector(trumpySpeech){
-  let count = 0
-//for every letter in trumpySpeech we increment our count if a vowel exist
-  for(let i = 0; i < trumpySpeech.length; i++) {
-    if (trumpySpeech[i] === 'vowels') {
-      count++
+  let vowelArray = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  let extraVowelsCount = 0
+  let regularPeopleVowelsCount = 0
+  let firstLetter = ""
+  let secondLetter = ""
+  for (let i = 0; i < trumpySpeech.length; i++) {
+    // reassigning here
+    secondLetter = trumpySpeech[i]
+    // check if letter repeats
+    if (secondLetter == firstLetter){
+      // check if letter is vowel
+      for (let j = 0; j < vowelArray.length; j++) {
+        if (secondLetter == vowelArray[j]) {
+          // vowel match!
+          // check for how many extra vowels
+
+        }
+      }
     }
+    firstLetter = secondLetter
   }
-  return count
 }
 
-//find the word with the most vowels
-function getMostVowels(vowels){
-  let trumpiest = ''
-  for(let i = 0; i < vowels.length, i++) {
-    if(countVowels(trumpySpeech[i]) > countVowels(vowels)) {
-      vowels = Math.round(trumpySpeech[i] * 1000)/1000
-    }
-  }
-  return trumpiest
-}
+trumpDetector(mexicans)
+
+
+// //count the vowels
+// function trumpDetector(trumpySpeech){
+//   let count = 0
+// //for every letter in trumpySpeech we increment our count if a vowel exist
+//   for(let i = 0; i < trumpySpeech.length; i++) {
+//     if (trumpySpeech[i] === vowels) {
+//       count++
+//     }
+//   }
+//   return count
+// }
+//
+// //find the word with the most vowels
+// function getMostVowels(vowels){
+//   let trumpiest = ''
+//   for(let i = 0; i < vowels.length, i++) {
+//     if(countVowels(trumpySpeech[i]) > countVowels(vowels)) {
+//       vowels = Math.round(trumpySpeech[i] * 1000)/1000
+//     }
+//   }
+//   return trumpiest
+// }
