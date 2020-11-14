c08298111f3c5f17d67229fd80862179074eee1a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-14 15:20:19 -0500

    Include file

---
+++ 7kyu/SimpleStringMatching/main.js
@@ -0,0 +1,8 @@
+//You will be given two strings a and b consisting of lower case letters, but a will have at most one asterix character. The asterix (if any) can be replaced with an arbitrary sequence (possibly empty) of lowercase letters. No other character of string a can be replaced. If it is possible to replace the asterix in a to obtain string b, then string b matches the pattern.
+
+let a = "aa*"
+let b = "aaa"
+
+function solve(a,b){
+    //..
+}
