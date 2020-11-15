9b060c6f6722ef07719bb26f53fe39d9eac9668c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-15 18:22:35 -0500

    Include code I got stuck and did not know how to create an object out of the verb and conjugate it's values into an array so I found a solution in python The code works but something is `undefinedo` lol ```js {   comer: [     'undefinedo',     'undefinedes',     'undefined',     'undefinedemos',     'undefineden'   ] } ```

--- 7kyu/latinAmericanSpanishConjugator/main.js
+++ 7kyu/latinAmericanSpanishConjugator/main.js
@@ -4,15 +4,47 @@
 //SUDO
 //-check if verb last 2 words end with ar || er || ir
 //-make the verb an object that has an empty array as avalue
-//-conjugate verb || change the last 2 letters and add values (conjugations) to array
+//-conjugate verb: change the last 2 letters && add values (conjugations) to array
 //-return object w/ it's conjugated words *in order
 
 //porpusly leaving out vosotros 'cause #decolonization boo~~
 
-let endsWithAr = ['o', 'as', 'a', 'amos', 'an']
-let endsWithEr = ['o', 'es', 'e', 'emos', 'en']
-let endsWithIr = ['o', 'es', 'e', 'imos', 'en']
-
 function conjugate(verb){
-  
+  let obj = {}
+//access letters
+  for(const letters of verb){
+//check if verb last 2 words end with ar || er || ir
+    if(verb.endsWith('er')){
+      obj[verb] = [
+        verb[0, - 2] + 'o',
+        verb[0, - 2] + 'es',
+        verb[0, - 1] + 'e',
+        verb[0, - 2] + 'emos',
+        verb[0, - 2] + 'en',
+      ]
+      return obj
+
+    } else if(verb.endsWith('ar')){
+      obj[verb] = [
+        verb[0, - 2] + 'o',
+        verb[0, - 2] + 'as',
+        verb[0, - 1] + 'a',
+        verb[0, - 2] + 'amos',
+        verb[0, - 2] + 'an',
+      ]
+      return obj
+
+    }else if(verb.endsWith('ir')){
+      obj[verb] = [
+        verb[0, - 2] + 'o',
+        verb[0, - 2] + 'es',
+        verb[0, - 1] + 'e',
+        verb[0, - 2] + 'imos',
+        verb[0, - 2] + 'en',
+      ]
+
+    }
+    return obj
+  }
 }
+conjugate('comer')
