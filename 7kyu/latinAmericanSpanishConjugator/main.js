02a225f55fdd2b1c78af6c2cbdc4b831a77c66cc
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-15 16:52:47 -0500

    Include file && sudo

---
+++ 7kyu/latinAmericanSpanishConjugator/main.js
@@ -0,0 +1,18 @@
+//Write a function called conjugate which will return an object with a spanish verb conjugated. The object must look like this:
+//{"comer": ["como","comes","come","comemos","coméis","comen"]}
+//Where the key is the verb in its original form (infinitive form) and its value will be an array with the conjugations.
+//SUDO
+//-check if verb last 2 words end with ar || er || ir
+//-make the verb an object that has an empty array as avalue
+//-conjugate verb || change the last 2 letters and add values (conjugations) to array
+//-return object w/ it's conjugated words *in order
+
+//porpusly leaving out vosotros 'cause #decolonization boo~~
+
+let endsWithAr = ['o', 'as', 'a', 'amos', 'an']
+let endsWithEr = ['o', 'es', 'e', 'emos', 'en']
+let endsWithIr = ['o', 'es', 'e', 'imos', 'en']
+
+function conjugate(verb){
+  
+}
