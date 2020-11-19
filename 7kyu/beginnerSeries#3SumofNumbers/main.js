bdde806ad78242d12164a8c7bb0e9ef3a98bdb1f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-19 17:54:47 -0500

    Include file and sudo

---
+++ 7kyu/beginnerSeries#3SumofNumbers/main.js
@@ -0,0 +1,19 @@
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between including them too and return it. If the two numbers are equal return a or b.
+
+//Note: a and b are not ordered!
+
+//SUDO:
+//put numbers in order
+//find numbers in between
+//add all the numbers
+//return the sum
+//OR return one of the nums if they're the same
+
+let nums = (23, -18)
+
+function getSum(a,b){
+
+}
+
+getSum(nums)
+console.log(nums)
