855acc108ccbea4a38c15b9e3316687691804542
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-19 19:01:56 -0500

    yup

--- 7kyu/beginnerSeries#3SumofNumbers/main.js
+++ 7kyu/beginnerSeries#3SumofNumbers/main.js
@@ -11,9 +11,9 @@
 
 
 const getSum = (a, b) => {
-  let min = Math.min(a, b),
-      max = Math.max(a, b)
-  return (max - min + 1) * (min + max) / 2
+  let min = Math.min(a,b)
+      max = Math.max(a,b)
+  return((max - min + 1) * (min + max) / 2)
 }
 
 
