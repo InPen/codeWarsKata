ce2cab7721e27b0eb9fad2ccc884ef1935f39836
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-22 22:36:32 -0500

    Include file

---
+++ 7kyu/mumbling/main.js
@@ -0,0 +1,8 @@
+//Examples:
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+	// your code
+}
