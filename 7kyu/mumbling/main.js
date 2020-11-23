1f2ea0093847de97920ce91baab2e0101f91a1d8
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-23 14:16:11 -0500

    Include solution

--- 7kyu/mumbling/main.js
+++ 7kyu/mumbling/main.js
@@ -3,6 +3,12 @@
 //accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 //accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
+//sudo
+//capitalize every letter
+//duplicate in lower case each letter
+
 function accum(s) {
-	// your code
+//.split('')makes every letter in the string have it's own index
+  s.split('').map((character, index) => (character.toUpperCase() + character.toLowerCase().repeat(i))).join('-')
 }
+accum("abcd")
