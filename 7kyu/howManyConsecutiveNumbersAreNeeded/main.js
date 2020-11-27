00ab293a5d3d4d53cece44e47e0b862885b31844
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-27 14:38:12 -0500

    My response that passes 4 test but when numbers get bigger it breaks

--- 7kyu/howManyConsecutiveNumbersAreNeeded/main.js
+++ 7kyu/howManyConsecutiveNumbersAreNeeded/main.js
@@ -3,6 +3,20 @@
 //For example:
 //If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Numbers in arr will be unique.
 
+// sudo:
+// put array in order
+// ??check what numbers are mising to make it consecutive
+
 function consecutive(arr) {
-//code me
+  let missing = []
+  let orderedArr = arr.sort()
+  for (let i = 0; i < orderedArr.length; i++){
+    for (let j = 1; j < 100 ; j++ ){
+      if(orderedArr[i] + j < orderedArr[i+1]) {
+        missing.push(orderedArr[i] + j)
+      }
+    }
+  }
+  console.log(missing) //missing.length
 }
+consecutive([1, -11, 113])
