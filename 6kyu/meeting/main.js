2e07f5c3090e481f1fbf0ff38718ebd1ae0c9c5d
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-29 23:41:11 -0500

    Include file

---
+++ 6kyu/meeting/main.js
@@ -0,0 +1,4 @@
+//make a program that
+//makes this string uppercase
+//gives it sorted in alphabetical order by last name.
+//When the last names are the same, sort them by first name. Last name //and first name of a guest come in the result between parentheses //separated by a comma.
