e299cc9e3bc202e855bad2c11863250502d563c4
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-30 23:27:35 -0500

    Potential response

--- 7kyu/TrainingJS#30:methodsofarrayObject---reduce()andReduceRight()/index.js
+++ 7kyu/TrainingJS#30:methodsofarrayObject---reduce()andReduceRight()/index.js
@@ -1,8 +1,9 @@
-
 function tailAndHead(arr){
-  let tail = [...arr].toString().split('').pop(  )
-  let head =
-  arr.map((tail, head) => tail + head).reduce((acc, currentValue) => )
+  arr.reduce((accum, currentValue) => {
+    let tail = accum.toString()
+    let head = currentValue.toString()
+    res = res * (parseInt(tail[tail.length - 1]) + parseInt(head[0]), 1)
+    console.log(res)
+  })
 
-}
 tailAndHead([123,456,789,12,34,56,78]) //532350
