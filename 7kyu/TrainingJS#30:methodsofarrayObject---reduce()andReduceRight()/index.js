4f5b48ac119ce8919c22285453e6ed858b6af0f9
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-30 21:30:41 -0500

    Change name

---
+++ 7kyu/TrainingJS#30:methodsofarrayObject---reduce()andReduceRight()/index.js
@@ -0,0 +1,8 @@
+
+function tailAndHead(arr){
+  let tail = [...arr].toString().split('').pop(  )
+  let head =
+  arr.map((tail, head) => tail + head).reduce((acc, currentValue) => )
+
+}
+tailAndHead([123,456,789,12,34,56,78]) //532350
