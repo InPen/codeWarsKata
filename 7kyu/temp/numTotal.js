5a6a742a89aa9d69f9c431ed31bd57b8fd43bab1
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-01 21:21:23 -0500

    File has been renamed

--- 7kyu/temp/numTotal.js
+++
@@ -1,7 +0,0 @@
-//Turn an array of numbers into a total of all the numbers
-
-function total(arr) {
-   console.log(arr.reduce((accum, currentValue) => accum + currentValue))
-}
-
-console.log(total([1,2,3])); // 6
