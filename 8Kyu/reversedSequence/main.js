81904a7377b70f2aed6a0d47397762c516654e33
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-03 14:13:40 -0500

    ES5 solution

--- 8Kyu/reversedSequence/main.js
+++ 8Kyu/reversedSequence/main.js
@@ -2,15 +2,18 @@
 //from n to 1 where n > 0
 //P : n
 //R : [nums]
-//E : [18, 12, 14, 9] [9.99, 3]
+//E : n = 8 [8,7,6,5,4,3,2,1]
 //P : declare a function
 //function takes a number
 //creates an array based of n
 //the numbers that are created n > 0
 
 function reverseSeq(n){
-//n is an elelement of the arr thats why it's in that syntax
-//ES6 uses Array word to turn any data type into an array
-//map will loop through our array and create a new array populated w/ new values
-  [...Array(n)].map((x,i) => n - 1)
+  let arr = []
+
+  for(let i = n; i >= 1; i--){
+    arr.push(i)
+  }
+  return arr
 }
+console.log(reverseSeq(8))
