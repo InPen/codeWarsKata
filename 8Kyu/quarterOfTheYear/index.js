2f109c06ee74aec17c0d840680adc822ada99553
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-09 13:48:04 -0500

    Include PREP

--- 8Kyu/quarterOfTheYear/index.js
+++ 8Kyu/quarterOfTheYear/index.js
@@ -2,8 +2,15 @@
 
 //For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
+// P : a month as an integer / real whole number
+// R : the quarter of the year the month is on
+// E : so the month october would be represented as 10 and I would return 4
+//     since it's on the 4th quarter of the year
+// P : declare the cuarters of the month
+//     create a conditional that checks what quarter the num we receive is on 
+
 const quarterOf = (month) => {
   // Your code here
 
 }
-quarterOf(8), 3)
+quarterOf(8)
