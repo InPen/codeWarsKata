9a2ce08779b2617178f5c92e179df24e37e3fb2e
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-19 11:12:09 -0500

    Include psudo

---
+++ 8Kyu/beginnerSeries#1SchoolPaperwork/index.js
@@ -0,0 +1,10 @@
+//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+//Your task is to calculate how many blank pages do you need
+
+//P : classmates = n && pages = m
+//R : how many pages do i need
+//E : paperwork(5, 5) == 25
+//P: I need the same amount of pages as there are class classmates
+  //so if I have 12 class mates, I need 12 pages
+  //then, based on the example, I multiply 12 * 12 and my answear is the result
