af31e72f416ce90fa21a93fd810d19bd48c8ae4c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-22 11:29:07 -0500

    include response

--- 7kyu/charCodeCalculation/index.js
+++ 7kyu/charCodeCalculation/index.js
@@ -1,1 +1,15 @@
-https://www.codewars.com/kata/57f75cc397d62fc93d000059/train/javascript
+//Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+//'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+
+const calc = string =>{
+  let sum = 0
+  let charCodes = string.split("").map(el => el.charCodeAt()).join("")
+  charCodes.split("").forEach(num => {
+    if(num == '7') {
+      sum += 6
+    }
+  })
+  console.log(sum)
+}
+calc('ABC')
