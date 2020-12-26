3e9178e168ab70ce4fcf309805886b9732eb78a0
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-26 10:12:01 -0500

    Include psudo code

---
+++ 7kyu/findTheMiddleElement/index.js
@@ -0,0 +1,12 @@
+//create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+//The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+//For example:
+
+//gimme([2, 3, 1]) => 0
+
+P : an Array of nums 
+R : retun the index of the number that lies in the middle of the other 2 elements 
+E : gimme([5,10,14]) => 10 || 1
+P :
