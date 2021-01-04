f8482e300069f8d8bb61b5d680f459f1dc547db1
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-03 23:15:36 -0500

    include file

--- 7kyu/highestAndLowest/main.js
+++ 7kyu/highestAndLowest/main.js
@@ -2,3 +2,13 @@ const highestToLowest = numbers => {
   let numArr = numbers.split('').sort((a,b) => b-a)
   return numArr[numArr.length - 1] + " " + numArr[0]
 }
+
+
+
+//
+
+function highAndLow(numbers){
+  let numArr = numbers.split('').sort((a,b) => b-a)
+  return Math.max(numArr) + " " + Math.min(numArr)
+
+}
