a362355caf00ab6a47d578bb4beadc7862c59106
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-07 16:15:22 -0500

    Include edits

---
+++ 7kyu/simpleFunMissingValues/index.js
@@ -0,0 +1,12 @@
+You are given an array of positive ints where every element appears three times, except one that appears only once (let's call it x) and one that appears only twice (let's call it y).
+
+Your task is to find x * x * y.
+
+Example
+For arr=[1, 1, 1, 2, 2, 3], the result should be 18
+
+3 x 3 x 2 = 18
+
+el 1 = x
+el 2 = y
+
