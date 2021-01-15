6f53ee2a7921c7b9022107a1ab534bde9b292940
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-15 13:31:01 -0500

    Include first solution

--- 8Kyu/sumTheStrings/index.js
+++ 8Kyu/sumTheStrings/index.js
@@ -1,3 +1,20 @@
-https://www.codewars.com/kata/5966e33c4e686b508700002d
+// https://www.codewars.com/kata/5966e33c4e686b508700002d
+// Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
 
-PREP 
+// sumStr("4", "5")    // should output "9"
+
+// P : 2 + string ints
+// R : return the sum of the strings as a string
+// E : "2" + "2" = "4"
+// P : turn strings into ints
+//     add
+//     turn sum back into string 
+
+const sumStr = (a,b) => {
+    let intA = parseInt(a)
+    let intB = parseInt(b)
+    let sum = intA + intB
+
+    return String(sum)
+}
+sumStr('10', '8')
