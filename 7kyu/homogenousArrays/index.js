ae396cc651dc931ce1eb1167e06ef63f19f5204f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-16 09:54:10 -0500

    Include PREP and starter code

--- 7kyu/homogenousArrays/index.js
+++ 7kyu/homogenousArrays/index.js
@@ -1,10 +1,20 @@
-https://www.codewars.com/kata/57ef016a7b45ef647a00002d
+// https://www.codewars.com/kata/57ef016a7b45ef647a00002d
 
-Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
+// Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
 
-Example:
+// Example:
 
-Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]]
-your function should return [[1, 5, 4], ['b']]
+// Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]]
+// your function should return [[1, 5, 4], ['b']]
 
-PREP
+// P : a multi dimentional arr
+// R : an arr with only values that are the same
+// E : [[123, 234, 432], ['', 'abc'], [''], ['', 1], ['', '1'], []]
+//     [[123, 234, 432], ['', 'abc'], [''], ['', '1']]
+// P : arrays[1] //access indexes
+//     sort arrs by typeof()
+//     remove arrs that don't have the same type
+
+const filterHomogenous = arrays => {
+    arrays.filter(index => typeOf(index === Number))
+}
