69cd41fef8eeb5aabf2ecfdd496cebe7d6935e28
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-21 16:09:48 -0500

    Include PREP

---
+++ 8Kyu/sumOfPositive/index.js
@@ -0,0 +1,13 @@
+You get an array of numbers, return the sum of all of the positives ones.
+
+P : arr of nums
+R : the sum of all + ints || 0
+E : [0,4,-3,10] => 14 
+P : filter arr
+    sum + ints
+    create a conditional for null or - ints
+
+const positiveSum = arr => {
+    
+} 
+positiveSum([0,4,-3,10])
