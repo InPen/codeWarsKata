5be9804b3a73e094d5487a7d151748456ba91d30
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-01 23:53:49 -0500

    Include return

--- 8Kyu/sumOfPositive/index.js
+++ 8Kyu/sumOfPositive/index.js
@@ -8,7 +8,8 @@
 //     create a conditional for null or - ints
 
 const positiveSum = arr => {
-    arr.filter(nums => nums > 0).reduce((accum, current) => accum + current, 0)
+
+    return arr.filter(nums => nums > 0).reduce((accum, current) => accum + current, 0)
 } 
 
 positiveSum([0,4,-3,10])
