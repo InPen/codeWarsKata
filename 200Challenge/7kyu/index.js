efc165026efdece68e867b1fc2b268a345edf275
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-17 19:32:43 -0500

    Include challenges solved thus far

--- 200Challenge/7kyu/index.js
+++ 200Challenge/7kyu/index.js
@@ -1,4 +1,24 @@
 Max-min arrays: https://www.codewars.com/kata/5a090c4e697598d0b9000004
+
+
+function solve(arr){
+    //..
+  arr.sort((a,b) => a - b)
+  let ans = [];
+  
+  
+  while(arr.length >= 1){
+    ans.push(arr.pop());
+    if(arr.length > 0){
+      ans.push(arr.shift())
+    }
+    
+  }
+  return ans
+  
+};
+
+
 Minimize Sum Of Array: https://www.codewars.com/kata/5a523566b3bfa84c2e00010b
 Maximum Product: https://www.codewars.com/kata/5a4138acf28b82aa43000117
 Alphabet symmetry: https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0
