17109ca07268f7a37d0bcbac5396157697b5c9c7
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-21 12:15:09 -0500

    Include PREP

--- 200Challenge/8kyu/sumMixedArray/index.js
+++ 200Challenge/8kyu/sumMixedArray/index.js
@@ -1,3 +1,10 @@
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return your answer as a number.
+// Return your answer as a number.
+
+// P : an arr of string and nums 
+// R : returning the sum of all nums and string nums 
+// E : [1,'1',1,'1'] => 4
+// P : turn our strings into nums 
+//     add all nums 
+//     return the total 
