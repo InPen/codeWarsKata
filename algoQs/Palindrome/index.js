ea8d6d4a23a563ad3a8e65e55b2941a05adbf83d
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-23 15:57:40 -0500

    Include file

---
+++ algoQs/Palindrome/index.js
@@ -0,0 +1,13 @@
+// A palindrome is a word or phrase that reads the same backward as forward. Write a function that checks for this.
+
+// P : a string 
+// R : return true or false if string is palindrome or not
+// E : "bananas" => false // "anna" => true
+// P : i can check first and last indexes and if they match is a palyndrome 
+//     turn my string into arr
+
+const palindromeChecker = string =>{
+    string.split('').toLowerCase()
+    console.log(string)
+}
+palindromeChecker('Anna')
