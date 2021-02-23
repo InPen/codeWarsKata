9ae769f2a664da62ffee5f88fb93cce077110f14
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-22 22:53:36 -0500

    Include prep

--- algoQs/stringReversal/index.js
+++ algoQs/stringReversal/index.js
@@ -1,1 +1,11 @@
-Given a string of characters as input, write a function that returns it with the characters reversed.
+// Given a string of characters as input, write a function that returns it with the characters reversed.
+
+// P : string of chars 
+// R : return chars reversed
+// E : "snooky" => "ykoons"
+// P : reverse method 
+//     split my string => Array
+//     loop every index 
+//     lastIndexOf() ?
+//     push to new Array
+//     join that Array to turn back as string 
