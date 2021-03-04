e9a21f30d0bc0d92491d47f271a0b564c0cafe75
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-04 12:04:32 -0500

    Change file name na dtry to re-solve the challenge

---
+++ 8Kyu/countingSheep/main.js
@@ -0,0 +1,37 @@
+
+//Counting sheep...
+//Consider an array/list of sheep where some sheep may be missing from their place.
+//We need a function that counts the number of sheep present in the array (true means present).
+
+let listOfSheep = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+// listOfSheep.forEach(function(sheep) {
+// //iterate though the array and find only the truthy values
+//   if (sheep === true) {
+//     let sheepCount = []
+//     sheepCount.push(listOfSheep.filter(Boolean).length)
+//     console.log(`There's only ${sheepCount} sheep present`)
+
+//   }
+// })
