a441fb93579e5848e34763f914820db40bc0f3b4
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-05 12:48:55 -0500

    Include response

--- 8Kyu/returnNegative/index.js
+++ 8Kyu/returnNegative/index.js
@@ -1,1 +1,6 @@
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+const makeNegative = num => {
+  return -Math.abs(num)
+}
+makeNegative(666)
