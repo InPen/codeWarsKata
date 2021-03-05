5de2a4aa439bdedde930469052fafcd4a47f0a16
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-05 11:31:45 -0500

    Include challenge

---
+++ 8Kyu/generateRangeOfInts/index.js
@@ -0,0 +1,1 @@
+Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
