a8c30074c8a9a352c551218ecdabc13aaa892591
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-06 23:08:32 -0500

    Include challenge

---
+++ 8Kyu/beConciseIV/index.js
@@ -0,0 +1,9 @@
+// Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+
+
+function find(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) return i;
+  }
+  return "Not found";
+}
