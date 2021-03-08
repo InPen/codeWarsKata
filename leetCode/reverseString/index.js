542aefec4acceb9e086e78e3ac3b8fb49282257f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-08 13:32:16 -0500

    Include response

--- leetCode/reverseString/index.js
+++ leetCode/reverseString/index.js
@@ -1,13 +1,14 @@
-Write a function that reverses a string. The input string is given as an array of characters char[].
+// Write a function that reverses a string. The input string is given as an array of characters char[].
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-P : an array of strings 
-R : the same array but reversed
-E : ['hi'] => ['ih']
-P : split / reverse /return
+// P : an array of strings 
+// R : the same array but reversed
+// E : ['hi'] => ['ih']
+// P : split / reverse /return
 
 
 const reverseString = (s) => {
-    
+    console.log(s.map(el => el.split('').reverse()))
 }
+reverseString(['Snooky'])
