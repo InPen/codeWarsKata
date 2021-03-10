08324339815a5b5e13b03ff9bbfe961b863c929e
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-10 10:42:40 -0500

    include code thus far. I'm able to get the fizz and the buzz but I also need to print out nums in between

--- leetCode/fizzBuzz/index.js
+++ leetCode/fizzBuzz/index.js
@@ -1,3 +1,16 @@
 // Write a program that outputs the string representation of numbers from 1 to n.
 
 // But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+const fizzBuzz = n => {
+    for(let i = 0; i < n; i++){
+        return 'i'
+        if(n % 3 === 0 && n % 5 === 0){
+            return 'FizzBuzz'
+        } else if (n % 3 === 0){
+            return 'Fizz'
+        } else if (n % 5 === 0){
+            return 'Buzz'
+        }
+    }
+}
