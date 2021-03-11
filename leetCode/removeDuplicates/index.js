7b9339c54cd8f20025aabc74d63486b0c128d2f4
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-11 14:58:55 -0500

    Include challenge

---
+++ leetCode/removeDuplicates/index.js
@@ -0,0 +1,12 @@
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Clarification:
+
+// Confused why the returned value is an integer but your answer is an array?
+
+// Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+// Internally you can think of this:
+
