234ec9ba620f1bd2d688bcea352f29078b080dfd
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-11 15:44:39 -0500

    Include what I have thus far

--- leetCode/removeDuplicates/index.js
+++ leetCode/removeDuplicates/index.js
@@ -10,3 +10,10 @@
 
 // Internally you can think of this:
 
+
+
+const removeDuplicates = nums =>{
+    let uniqueNums = [...new Set(nums)]
+    console.log(uniqueNums);
+}
+nums([0,0,1,1,1,2,2,3,3,4])
