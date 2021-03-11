ddd9fe32fe96ec2a711ec6425858f03db93a96d0
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-11 14:37:53 -0500

    comment out challenge

--- leetCode/singleNumber/index.js
+++ leetCode/singleNumber/index.js
@@ -1,11 +1,11 @@
-Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+// Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
 
-P : arr of ints 
-R : return int that doesn't repeat 
-E : [1,3,3,1,5] =>
-P : 
+// P : arr of ints 
+// R : return int that doesn't repeat 
+// E : [1,3,3,1,5] =>
+// P : 
 
 
 const singleNumber = nums => {
