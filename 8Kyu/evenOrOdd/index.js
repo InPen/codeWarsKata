57f76fd8d5fd49c3847c17aa801b8b4a6c524d96
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-03-14 18:06:43 -0400

    include challenge

---
+++ 8Kyu/evenOrOdd/index.js
@@ -0,0 +1,23 @@
+// Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
+
+// If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
+
+// If the sum of odd numbers is greater than the sum of even numbers return: "Odd is greater than Even"
+
+// If the total of both even and odd numbers are identical return: "Even and Odd are the same"
+
+
+function evenOrOdd(str) {
+  let nums = str.split('')
+  let even = nums.filter(num => num % 2 === 0).reduce((accum,current) => accum + current, 0)
+  let odd = nums.filter(num => num % 3 === 0).reduce((accum, current) => accum + current, 0)
+  
+  if(even > odd){
+    return 'Even is greater than Odd'
+  }else if(odd > even){
+    return 'Odd is greater than Even'
+  }else{
+    return 'Even and Odd are the same'
+  }
+  
+}
