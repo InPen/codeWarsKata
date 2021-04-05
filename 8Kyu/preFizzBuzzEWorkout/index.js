4af3c9510f4f2a117d931d025f08a1a7daece334
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-05 13:32:59 -0400

    Incluse solution

---
+++ 8Kyu/preFizzBuzzEWorkout/index.js
@@ -0,0 +1,16 @@
+// This is the first step to understanding FizzBuzz.
+// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+// Your expected output is an array of positive integers from 1 to n (inclusive)
+// Your job is to write an algorithm that gets you from the input to the output.
+
+const preFizz = n => {
+    const nArr = []
+    n = 1
+
+    while(nArr.length < n){
+        nArr.push(num)
+        num += 1
+    }
+
+    console.log(nArr);
+}
