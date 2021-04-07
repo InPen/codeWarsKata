c48aa9151432c608a22bfce3f3ef38051ec5a8d0
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-07 11:04:05 -0400

    Add challenge

--- 7kyu/highestAndLowest/main.js
+++ 7kyu/highestAndLowest/main.js
@@ -1,3 +1,13 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+
+
 const highestToLowest = numbers => {
   let numArr = numbers.split('').sort((a,b) => b-a)
   return numArr[numArr.length - 1] + " " + numArr[0]
