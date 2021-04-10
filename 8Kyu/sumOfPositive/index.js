fb2ebf673033244cec939e95089820c4c0864d14
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-10 09:49:51 -0400

    Include loop, adding is incorrect

--- 8Kyu/sumOfPositive/index.js
+++ 8Kyu/sumOfPositive/index.js
@@ -2,12 +2,26 @@
 
 // P : arr of nums
 // R : the sum of all + ints || 0
-// E : [0,4,-3,10] => 14 
+// E : [0,4,-3,10] => 14
 // P : filter arr
 //     sum + ints
 //     create a conditional for null or - ints
 
-const positiveSum = arr => {
-    return arr.filter(nums => nums > 0).reduce((accum, current) => accum + current, 0)
-} 
-positiveSum([0,4,-3,10])
+// const positiveSum = arr => {
+
+//     return arr.filter(nums => nums > 0).reduce((accum, current) => accum + current, 0)
+// }
+// positiveSum([0,4,-3,10])
+
+const positiveSum = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let number = arr[i];
+    let totalPositive = 0;
+    if (number > 0) {
+      totalPositive += number
+      return totalPositive
+    } else {
+      return 0;
+    }
+  }
+};
