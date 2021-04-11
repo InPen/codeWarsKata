d91db5e7928f1a439b4d110ffc98932cfb595bbb
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-11 10:10:52 -0400

    include loopy solution

--- 8Kyu/reversedString/index.js
+++ 8Kyu/reversedString/index.js
@@ -2,9 +2,17 @@
 
 // 'world'  =>  'dlrow'
 
-
+function solution(str) {
+  return str.split("").reverse().join("");
+}
 
 
 function solution(str) {
-  return str.split("").reverse().join("");
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    let letter = str[i];
+    reversed += letter;
+    //     console.log(letter)
+  }
+  return reversed;
 }
