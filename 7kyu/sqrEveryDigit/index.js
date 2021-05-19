d2c5042bf2a1fdc29f769fa9ada97ead36471787
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-19 11:48:42 -0400

    Include response

---
+++ 7kyu/sqrEveryDigit/index.js
@@ -0,0 +1,9 @@
+function squareDigits(num) {
+  let nums = num.toString().split("");
+  let sqrArr = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    sqrArr.push(nums[i] * nums[i]);
+  }
+  return Number(sqrArr.join(""));
+}
