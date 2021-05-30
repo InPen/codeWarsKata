58e18b1918a94b641ae578ae80b18766a26f5ac2
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-29 21:50:00 -0400

    idea on how to solve

--- 6kyu/findTheOddInt/index.js
+++ 6kyu/findTheOddInt/index.js
@@ -2,6 +2,8 @@
 // There will always be only one integer that appears an odd number of times.
 // 
 
+push each num into a variable to check if it appears an odd amount of times?
+
 function findOdd(A) {
   //happy coding!
   return 0;
