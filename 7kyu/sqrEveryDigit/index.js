471b3b2e5eb4e9374348391d3395f504c8e86f9e
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-06-15 15:35:22 -0400

    intentional empty commit

--- 7kyu/sqrEveryDigit/index.js
+++ 7kyu/sqrEveryDigit/index.js
@@ -2,8 +2,10 @@ function squareDigits(num) {
   let nums = num.toString().split("");
   let sqrArr = [];
 
-  for (let i = 0; i < nums.length; i++) {
+  fr (let i = 0; i < nums.length; i++) {
     sqrArr.push(nums[i] * nums[i]);
   }
   return Number(sqrArr.join(""));
 }
+
+
